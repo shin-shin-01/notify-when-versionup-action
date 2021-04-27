b3e0672838860fe4e93164a5c09f3b514f8db94d
Kaze Shindo <kaze.bknb@outlook.jp>
2021-04-27 17:11:16 +0900

    rm unused cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,7 @@
 
 echo "Hello, World in entrypoint.sh"
 
-echo "===================="
-pwd
-
 echo "ls ====================="
 ls -a
-echo "ls / ===================="
-ls -a /
-echo "ls /github =============="
-ls -a /github
-echo "ls /github/workspace ===="
-ls -a /github/workspace
-echo "ls /github/workflow ======"
-ls -a /github/workflow
 
 python /myapp/src/main.py
