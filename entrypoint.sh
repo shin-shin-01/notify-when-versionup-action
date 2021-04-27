9eff3c5a9da156275047731ce5e16a9e5d9d6e63
Kaze Shindo <kaze.bknb@outlook.jp>
2021-04-27 21:06:13 +0900

    fix: IFS in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,14 @@ echo "Hello, World in entrypoint.sh"
 # ex) ./test/Dockerfile:2:# https://api.github.com/repos/octocat/hello-world/issues/42
 FILE_COMMENT_RESULT=`find . -type f -print | xargs grep -s -n "#[^\n]*https://api.github.com/repos/[^/\n]*/[^/\n]*/issues/\d*" `
 
-# 改行コード\n で split
-IFS=$'\n'
+# 改行 で split
+# $'\n' not working in docker
+IFS=$'
+'
+
 for result in $FILE_COMMENT_RESULT
 do
 
-python src/main.py $result
+python /myapp/src/main.py $result
 
 done
