c2544e6ccdd8e9d9509340d793cfa2633d48a69c
Kaze Shindo <kaze.bknb@outlook.jp>
2021-04-27 18:59:04 +0900

    get TargetPath and print in python

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,19 @@
 
 echo "Hello, World in entrypoint.sh"
 
-echo "ls ====================="
-ls -a
 
-python /myapp/src/main.py
+# TODO: add release url
+
+# コメントを検出する
+# - 現状ではDockerfileに限らず取得
+# ex) ./test/Dockerfile:2:# https://api.github.com/repos/octocat/hello-world/issues/42
+FILE_COMMENT_RESULT=`find . -type f -print | xargs grep -s -n "#[^\n]*https://api.github.com/repos/[^/\n]*/[^/\n]*/issues/\d*" `
+
+# 改行コード\n で split
+IFS=$'\n'
+for result in $FILE_COMMENT_RESULT
+do
+
+python src/main.py $result
+
+done
