41c62bbef64abc2218965e4e1ed1fa276c61292e
Kaze Shindo <kaze.bknb@outlook.jp>
2021-04-28 12:58:25 +0900

    update issue link

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ echo "Hello, World in entrypoint.sh"
 
 # コメントを検出する
 # - 現状ではDockerfileに限らず取得
-# ex) ./test/Dockerfile:2:# https://api.github.com/repos/octocat/hello-world/issues/42
-FILE_COMMENT_RESULT=`find . -type f -print | xargs grep -s -n "#[^\n]*https://api.github.com/repos/[^/\n]*/[^/\n]*/issues/\d*" `
+# ex) ./test/Dockerfile:2:# https://github.com/shin-shin-01/github-test/issues/2
+FILE_COMMENT_RESULT=`find . -type f -print | xargs grep -s -n "#[^\n]*https://github.com/[^/]*/[^/]*/issues/\d*" `
 
 # 改行 で split
 # $'\n' not working in docker
--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 import sys, re
 from typing import List, Union
 
-TARGET = "https://api.github.com/repos/[^/\n]*/[^/\n]*/issues/\d*"
+TARGET = "https://github.com/([^/]*)/([^/]*)/issues/(\d*)"
 
 def main():
-    file_path, line, target_path = split_grep_result(sys.argv[1])
-    print(file_path, line, target_path)
+    file_path, line, target_info = split_grep_result(sys.argv[1])
+    print(file_path, line, target_info)
 
 
 """
 split grep_result
-return file_path, line, target_path
+return file_path, line, target_info
 
 ex)
-input ./test/Dockerfile:2:# https://api.github.com/repos/octocat/hello-world/issues/42
-return ./test/Dockerfile, 2, https://api.github.com/repos/octocat/hello-world/issues/42
+input ./test/Dockerfile:2:# https://github.com/shin-shin-01/github-test/issues/2
+return ./test/Dockerfile, 2, ( shin-shin-01, github-test, 2 )
 """
 def split_grep_result(grep_result: str) -> List[Union[str, str, str]]:
     # ファイルのパスと行数を取得
@@ -22,9 +22,9 @@ def split_grep_result(grep_result: str) -> List[Union[str, str, str]]:
     # ターゲットとなるパスを取得
     # - 取得条件が変化する可能性があるため個別に取得
     # - 複数存在する場合はひとつめのみ取得
-    target_path = re.findall(f'{TARGET}', grep_result)
+    target_info = re.findall(f'{TARGET}', grep_result)
 
-    return filepath_line_split[0][0], filepath_line_split[0][1], target_path[0]
+    return filepath_line_split[0][0], filepath_line_split[0][1], target_info[0]
 
 
 if __name__ == "__main__":
