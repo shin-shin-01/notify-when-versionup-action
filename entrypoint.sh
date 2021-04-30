5063ccdb171126b0865897770e843ef012734cc6
Kaze Shindo <kaze.bknb@outlook.jp>
2021-04-30 17:14:19 +0900

    get only Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,8 @@ GITHUB_DEFAULT_BRANCH=$3
 # TODO: add release url
 
 # コメントを検出する
-# - 現状ではDockerfileに限らず取得
 # ex) ./test/Dockerfile:2:# https://github.com/shin-shin-01/github-test/issues/2
-FILE_COMMENT_RESULT=`find . -type f -print | xargs grep -s -n "#[^\n]*https://github.com/[^/]*/[^/]*/issues/\d*" `
+FILE_COMMENT_RESULT=`find . -name Dockerfile | xargs grep -s -n "#[^\n]*https://github.com/[^/]*/[^/]*/issues/\d*" `
 
 # 改行 で split
 # $'\n' not working in docker
