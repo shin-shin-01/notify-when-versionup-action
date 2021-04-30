9cf2bd0e93a09c52129d6101fcb3327776745b11
Kaze Shindo <kaze.bknb@outlook.jp>
2021-04-30 18:42:54 +0900

    add function to process release url

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,10 @@ GITHUB_TOKEN=$1
 GITHUB_REPOSITORY=$2
 GITHUB_DEFAULT_BRANCH=$3
 
-# TODO: add release url
+# issue と release url が同一行に記載されていたら別々に処理できなくなるため
+# コメント検出を２回に分けて行い 処理を分ける
 
+# ISSUE ==========================================
 # コメントを検出する
 # ex) ./test/Dockerfile:2:# https://github.com/shin-shin-01/github-test/issues/2
 FILE_COMMENT_RESULT=`find . -name Dockerfile | xargs grep -s -n "#[^\n]*https://github.com/[^/]*/[^/]*/issues/\d*" `
@@ -21,6 +23,23 @@ IFS=$'
 for result in $FILE_COMMENT_RESULT
 do
 
-python /myapp/src/main.py $GITHUB_TOKEN $GITHUB_REPOSITORY $GITHUB_DEFAULT_BRANCH $result
+python /myapp/src/main.py $GITHUB_TOKEN $GITHUB_REPOSITORY $GITHUB_DEFAULT_BRANCH "issue" $result
+
+done
+
+# RELEASE ==========================================
+# コメントを検出する
+# ex) ./test/Dockerfile:2:# https://github.com/shin-shin-01/github-test/releases
+FILE_COMMENT_RESULT=`find . -name Dockerfile | xargs grep -s -n "#[^\n]*https://github.com/[^/]*/[^/]*/releases" `
+
+# 改行 で split
+# $'\n' not working in docker
+IFS=$'
+'
+
+for result in $FILE_COMMENT_RESULT
+do
+
+python /myapp/src/main.py $GITHUB_TOKEN $GITHUB_REPOSITORY $GITHUB_DEFAULT_BRANCH "release" $result
 
 done
--- src/func.py
+++ src/func.py
@@ -1,7 +1,12 @@
 import re
 import requests
-from typing import List, Union
+from typing import List, Union, Tuple
 from datetime import datetime
+
+TARGET = {
+    "issue": "https://github.com/([^/]*)/([^/]*)/issues/(\d*)",
+    "release": "https://github.com/([^/]*)/([^/]*)/releases"
+}
 
 """
 split grep_result
@@ -11,13 +16,13 @@ ex)
 input ./test/Dockerfile:2:# https://github.com/shin-shin-01/github-test/issues/2
 return ./test/Dockerfile, 2, ( shin-shin-01, github-test, 2 )
 """
-def split_grep_result(TARGET: str, grep_result: str) -> List[Union[str, int, str]]:
+def split_grep_result(target_type: str, grep_result: str) -> List[Union[str, int, Tuple[str, ...]]]:
     # ファイルのパスと行数を取得
     filepath_line_split = re.findall('([^:]*):([^:]*)', grep_result)
     # ターゲットとなるパスを取得
     # - 取得条件が変化する可能性があるため個別に取得
     # - 複数存在する場合はひとつめのみ取得
-    target_info = re.findall(f'{TARGET}', grep_result)
+    target_info = re.findall(TARGET[target_type], grep_result)
 
     return filepath_line_split[0][0], int(filepath_line_split[0][1]), target_info[0]
 
